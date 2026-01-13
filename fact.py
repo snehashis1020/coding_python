44dd7e4397bade59ab26199d5c4fef051c8f8e07
snehashis1020 <babanpaul9090@gmail.com>
2026-01-13 19:20:11 +0530

    Update fact.py

--- fact.py
+++ fact.py
@@ -1,11 +1,6 @@
-def fact(n):
-    if n==0:
-        return 1
-    else:
-        return n*fact(n-1)
-
-n=int(input("enter the number"))
-    
-
-result=fact(n)
-print("factorial is",result)
+def factorial(n):
+    fact = 1
+    for i in range(1, n + 1):
+        fact = fact * i
+    return fact
+print("Factorial:", factorial(5))
