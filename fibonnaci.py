01bb9ccbc3d0ec442a68cb9307553dc6947eef31
snehashis1020 <babanpaul9090@gmail.com>
2026-01-13 19:25:34 +0530

    Update fibonnaci.py

--- fibonnaci.py
+++ fibonnaci.py
@@ -1,10 +1,10 @@
-n = 6
-
-a = 0
-b = 1
-
-for i in range(n):
-    print(a, end=" ")
-    c = a + b
-    a = b
-    b = c
+def fibonacci(n):
+    a = 0
+    b = 1
+    for i in range(n):
+        print(a, end=" ")
+        c = a + b 
+        a = b
+        b = c
+    print()
+fibonacci(5)
