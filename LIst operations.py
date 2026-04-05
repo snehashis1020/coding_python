04c682ae6a876a45e1e65b6b6f7824bc261f0405
snehashis1020 <babanpaul9090@gmail.com>
2026-04-05 22:44:58 +0530

    Update LIst operations.py

--- LIst operations.py
+++ LIst operations.py
@@ -1,31 +1,25 @@
 # 1
 my_list = [10, 20, 30, 40, 50]
 print("the list is:", my_list)
-
 #  2
 fruits = ["apple", "banana", "mango"]
 print("first fruit:", fruits[0])
 print("third fruit:", fruits[2])
-
 #  3
 numbers = [1, 2, 3, 4]
 numbers[2] = 10
 print("modified list:", numbers)
-
-
-# 🔹 4
+#  4
 print("4")
 numbers = [1, 2, 3]
 numbers.append(5)
 numbers.remove(2)
 print("updated list:", numbers)
-
 # 6
 list1 = [5, 10, 15, 20]
 list2 = list1[:]   
 print("original list:", list1)
 print("copied list:", list2)
-
 #  7
 list1 = [5, 10, 15, 20]
 list2 = list(list1)   
